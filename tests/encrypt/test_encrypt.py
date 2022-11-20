ec53d12933f8d16d9c2a32469e137ae54a886c66
prtpj1 <prtpj1@gmail.com>
2022-11-20 20:17:32 -0300

    :mask: tests:(Requisite 02) - add test_encypt :arrow_right: test the method encrypt_message which invert a word with some rules

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    MESSAGE = "AABBCC"
+    INVALID_MSG = 5
+    INVALID_KEY = "K"
+
+    assert encrypt_message(MESSAGE, 3) == "BAA_CCB"
+    assert encrypt_message(MESSAGE, 4) == "CC_BBAA"
+    assert encrypt_message(MESSAGE, -1) == "CCBBAA"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(MESSAGE, INVALID_KEY)
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(INVALID_MSG, 3)
