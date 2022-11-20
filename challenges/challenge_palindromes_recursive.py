81285b0da17616d8c8b4e00c592119c4d485331c
prtpj1 <prtpj1@gmail.com>
2022-11-20 17:15:50 -0300

    :yum: feat:(Requisite 03) - add method is_palindrome_recursive :arrow_right: the method check if a word is a palindrome or not

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or word == "":
+        return False
+
+    if low_index >= high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+if __name__ == "__main__":
+    print(is_palindrome_recursive("SOCOS", 0, 4))
