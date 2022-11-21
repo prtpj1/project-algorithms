fbd5fc5eae17905d3feea6c74b1e99bd8f7e5fd1
prtpj1 <prtpj1@gmail.com>
2022-11-21 16:04:16 -0300

    :yum: feat:(Requisite 01) - add method study schedule :arrow_right: checks which time schedule there are more students online at same time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if not isinstance(target_time, int):
+        return None
 
-    raise NotImplementedError
+    students_online = 0
+
+    for period in permanence_period:
+        login, logout = period
+        if not isinstance(login, int) or not isinstance(logout, int):
+            return None
+        elif login <= target_time <= logout:
+            students_online += 1
+    return students_online
+
+
+if __name__ == "__main__":
+    students_time = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+    time = 5
+    print(study_schedule(students_time, time))
